9de25e29f1268ea1fd09bd6b8e1bb08adcc7470a
Dominic Amato <dominic.a.amato@gmail.com>
2016-12-07 02:10:56 -0600

    handle null pointer and better method overloading

--- src/main/java/com/rabbit/gui/utils/SkinManager.java
+++ src/main/java/com/rabbit/gui/utils/SkinManager.java
@@ -12,10 +12,7 @@ public class SkinManager {
 	private static Map<String, UUID> playerSkin = new HashMap<String, UUID>();
 
 	public static void addSkin(EntityPlayer player, String skin) {
-		UUID textureId = UUID.randomUUID();
-		// incase its a url the texture helper will sort it all out
-		TextureHelper.addTexture(textureId, skin);
-		playerSkin.put(player.getDisplayNameString(), textureId);
+		addSkin(player.getName(), skin);
 	}
 
 	public static void addSkin(String player, ResourceLocation skin) {
@@ -33,11 +30,7 @@ public class SkinManager {
 	}
 
 	public static boolean bindSkinTexture(EntityPlayer player) {
-		if (playerSkin.containsKey(player.getDisplayNameString())) {
-			TextureHelper.bindTexture(playerSkin.get(player.getDisplayNameString()));
-			return true;
-		}
-		return false;
+		return bindSkinTexture(player.getName());
 	}
 
 	public static boolean bindSkinTexture(String player) {
@@ -49,27 +42,24 @@ public class SkinManager {
 	}
 
 	public static String getSkinTexture(EntityPlayer player) {
-		if (playerSkin.containsKey(player.getName())) {
-			// not sure we can do anything about dynamic textures currently
-			if (TextureHelper.getDynamicTexture(playerSkin.get(player.getName())) == null) {
-				return TextureHelper.getStaticTexture(playerSkin.get(player.getName())).toString();
-			}
-		}
-		return null;
+		return getSkinTexture(player.getName());
 	}
 
 	public static String getSkinTexture(String player) {
 		if (playerSkin.containsKey(player)) {
 			// not sure we can do anything about dynamic textures currently
 			if (TextureHelper.getDynamicTexture(playerSkin.get(player)) == null) {
-				return TextureHelper.getStaticTexture(playerSkin.get(player)).toString();
+				ResourceLocation tex = TextureHelper.getStaticTexture(playerSkin.get(player));
+				if (tex != null) {
+					return tex.toString();
+				}
 			}
 		}
 		return null;
 	}
 
 	public static boolean hasSkinTexture(EntityPlayer player) {
-		return playerSkin.containsKey(player.getName());
+		return hasSkinTexture(player.getName());
 	}
 
 	public static boolean hasSkinTexture(String player) {
@@ -77,7 +67,7 @@ public class SkinManager {
 	}
 
 	public static void removeSkinTexture(EntityPlayer player) {
-		playerSkin.remove(player.getName());
+		removeSkinTexture(player.getName());
 	}
 
 	public static void removeSkinTexture(String player) {
@@ -85,14 +75,7 @@ public class SkinManager {
 	}
 
 	public static void setSkinTexture(EntityPlayer player, String skin) {
-		if (playerSkin.containsKey(player.getDisplayNameString())) {
-			UUID textureId = UUID.randomUUID();
-			// incase its a url the texture helper will sort it all out
-			TextureHelper.addTexture(textureId, skin);
-			playerSkin.replace(player.getDisplayNameString(), textureId);
-		} else {
-			addSkin(player, skin);
-		}
+		setSkinTexture(player.getName(), skin);
 	}
 
 	public static void setSkinTexture(String player, String skin) {
